a7abd833cabd41730530437d54a3703842e78605
LoryMorty <168710437+LoryMorty@users.noreply.github.com>
2025-10-11 12:50:45 +0000

    Revert "aggiunta di bootstrap"
    This reverts commit 0b96ce9073a645d011be795344a2772234f171e6.

--- mini-blog/src/app/app-module.ts
+++ mini-blog/src/app/app-module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { PostList } from './app/components/post-list/post-list';
+import { PostDetail } from './app/components/post-detail/post-detail';
+import { NewPostForm } from './app/components/new-post-form/new-post-form';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    PostList,
+    PostDetail,
+    NewPostForm
   ],
   imports: [
     BrowserModule,
---
+++ mini-blog/src/app/app/components/new-post-form/new-post-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-post-form',
+  standalone: false,
+  templateUrl: './new-post-form.html',
+  styleUrl: './new-post-form.css'
+})
+export class NewPostForm {
+
+}
