0b96ce9073a645d011be795344a2772234f171e6
LoryMorty <168710437+LoryMorty@users.noreply.github.com>
2025-10-11 12:22:02 +0000

    aggiunta di bootstrap

--- mini-blog/src/app/app-module.ts
+++ mini-blog/src/app/app-module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
-import { PostList } from './app/components/post-list/post-list';
-import { PostDetail } from './app/components/post-detail/post-detail';
-import { NewPostForm } from './app/components/new-post-form/new-post-form';
 
 @NgModule({
   declarations: [
-    App,
-    PostList,
-    PostDetail,
-    NewPostForm
+    App
   ],
   imports: [
     BrowserModule,
